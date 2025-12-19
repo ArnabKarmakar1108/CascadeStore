e26c1e3c2534b76f4b1ede45be262bc15ba528c8
Arnab Karmakar <arnabk1108@gmail.com>
2025-12-18 21:01:43 -0700

    Add SSTable flush loop writing data entries.

--- src/main/java/io/cascadestore/lsm/sstable/SSTable.java
+++ src/main/java/io/cascadestore/lsm/sstable/SSTable.java
@@ -66,9 +66,82 @@ public class SSTable {
     logger.info("Created SSTable: " + filePrefix);
   }
 
+  public SSTable(String directory, int level, long sequenceNumber) throws IOException {
+    this.level = level;
+    this.sequenceNumber = sequenceNumber;
+    this.sparseIndex = new TreeMap<>();
+
+    // Define file paths
+    String filePrefix = String.format("sst_L%d_S%d", level, sequenceNumber);
+    this.dataFilePath = Path.of(directory, filePrefix + ".data");
+    this.indexFilePath = Path.of(directory, filePrefix + ".index");
+    this.filterFilePath = Path.of(directory, filePrefix + ".filter");
+
+    // Load metadata
+    this.creationTime = new File(dataFilePath.toString()).lastModified();
+
+    // Load from disk
+    loadFromDisk();
+
+    logger.info("Opened SSTable: " + filePrefix);
+  }
+
   private void flushToDisk(MemTable memTable) throws IOException {
+    logger.info("Flushing MemTable to disk as SSTable");
+
+    // Create a bloom filter for efficient negative lookups
+    BloomFilter filter = new BloomFilter(memTable.getEntries().size(), 0.01);
+
+    // Create data and index files
+    try (FileChannel writeDataChannel =
+            FileChannel.open(
+                dataFilePath,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+        FileChannel writeIndexChannel =
+            FileChannel.open(
+                indexFilePath,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.TRUNCATE_EXISTING)) {
+
+      // Write header to data file
+      ByteBuffer headerBuffer = ByteBuffer.allocate(16);
+      headerBuffer.putLong(creationTime);
+      headerBuffer.putInt(level);
+      headerBuffer.putInt(memTable.getEntries().size());
+      headerBuffer.flip();
+      writeDataChannel.write(headerBuffer);
+
+      // Write entries to data file and build index
+      long currentOffset = 16; // Start after header
+
+      for (Map.Entry<ByteArrayWrapper, MemTable.ValueEntry> entry :
+          memTable.getEntries().entrySet()) {
+        byte[] key = entry.getKey().getData();
+        MemTable.ValueEntry valueEntry = entry.getValue();
+
+        // Skip expired entries
+        if (valueEntry.isExpired()) {
+          continue;
+        }
+
+        // Add key to bloom filter
+        filter.add(key);
+
+        // Create SSTableEntry
+        SSTableEntry sstEntry;
+        if (valueEntry.isTombstone()) {
+          sstEntry = SSTableEntry.tombstone(key, valueEntry.getExpirationTime());
+        } else {
+          sstEntry = SSTableEntry.of(key, valueEntry.getValue(), valueEntry.getExpirationTime());
+        }
+
+
+  private void loadFromDisk() throws IOException {
     throw new UnsupportedOperationException(
-        "flush path added in a follow-up commit");
+        "load path added in a follow-up commit");
   }
 
   // lookup path added in a follow-up commit
